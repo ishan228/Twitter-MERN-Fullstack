9c32c20dfe1dffd13c42acad46a3983cd7ca7718
Ishan Gautam <ishan0gautam0@gmail.com>
2024-12-03 18:37:03 +0545

    Added UserRoutes and controllers

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,17 @@
+import express, { Router } from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  followUnfollowUser,
+  getSuggestedUsers,
+  getUserProfile,
+  updateUser,
+} from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/profile/:username", getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/update", protectRoute, updateUser);
+
+export default router;
