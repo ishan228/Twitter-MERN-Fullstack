6ca125e9a5cafa9df3838553c4ead85a5f425e51
Ishan Gautam <ishan0gautam0@gmail.com>
2024-12-11 18:17:16 +0545

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
-import connectMongoDB from "./db/connectMongoDB.js";
-import { v2 as cloudinary } from "cloudinary";
 import notificationRoutes from "./routes/notification.route.js";
+
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -17,10 +20,13 @@ cloudinary.config({
 });
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -28,7 +34,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
-  console.log(`Server is running on Port:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
 });
